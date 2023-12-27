6cc0e1a3ca516bcb8c188c29ad98db3f3486fad3
Stefan Schwingenschloegl <stefan.roland.schwingenschloegl@gmail.com>
2023-12-27 14:10:44 +0100

    API_database_ingestion_pipeline initialized via kedro

---
+++ wl-data-engineering/src/wl_data_engineering/pipelines/API_database_ingestion_pipeline/nodes.py
@@ -0,0 +1,4 @@
+"""
+This is a boilerplate pipeline 'API_database_ingestion_pipeline'
+generated using Kedro 0.19.1
+"""
---
+++ wl-data-engineering/src/wl_data_engineering/pipelines/API_database_ingestion_pipeline/pipeline.py
@@ -0,0 +1,10 @@
+"""
+This is a boilerplate pipeline 'API_database_ingestion_pipeline'
+generated using Kedro 0.19.1
+"""
+
+from kedro.pipeline import Pipeline, pipeline
+
+
+def create_pipeline(**kwargs) -> Pipeline:
+    return pipeline([])
